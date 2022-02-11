701936d8e33226799481e727f71adac6f6294ae0
barjenbruchlaw <brian@kcpropertylaw.com>
2022-02-11 11:43:51 -0600

    Select PM and Owner

--- src/pages/addcase.js
+++ src/pages/addcase.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PageNavBar from '../components/PageNavBar'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import '../App.css'
 
 const Addcase = () => {
@@ -16,8 +16,46 @@ const Addcase = () => {
         <PageNavBar />
         </div>
 
-        <main className='page-main'>
+        <main className='page-main py-5'>
         <h1 className='text-center'>Add Case</h1>
+
+        <Container>
+            <Row>
+                <Col className='p-5'>
+
+{/* This should populate from the property manager assigned to the user; attorneys and paralegals can access all */}
+
+                <Form.Select aria-label="Select PM">
+                    <option>Select Property Management Company</option>
+                    <option value="1">ABC Company</option>
+                    <option value="2">JKL Properties</option>
+                    <option value="3">XYZ Asset Management</option>
+                </Form.Select>
+                </Col>
+                <Col className='p-5'>
+
+{/* This should populate from the property owners associated with the property manager and assigned to the user; attorneys and paralegals can access all assigned to the manager */}
+
+                <Form.Select aria-label="Select Owner">
+                    <option>Select Property Owner</option>
+                    <option value="1">123 Main, LLC</option>
+                    <option value="2">456 Main, LLC</option>
+                    <option value="3">789 Main, LLC</option>
+                </Form.Select>
+                
+                </Col>
+            </Row>
+
+            <Row>
+            <Col className='px-5'></Col>
+            <Col>
+            <Button variant="dark" disabled className='btn px-5'>Back</Button><Button variant="dark" className='btn mx-5 px-5'>Next</Button>
+            
+            </Col>
+
+            </Row>
+        </Container>
+
         </main>
 
     </div>
